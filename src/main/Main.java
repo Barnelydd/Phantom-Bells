e9eecaf60d6edb41de7de87c7f342c84e24fffaa
B. D. <164544260+Barnelydd@users.noreply.github.com>
2025-11-15 11:28:29 -0400

    Tutorial hell

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Heavens sang in ding dong dang");
+		
+		window.setLocationRelativeTo(window);
+		window.setVisible(true);
+		
+		
+
+	}
+
+}
