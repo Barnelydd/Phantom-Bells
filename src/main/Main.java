f7c0eba4d988aba50d392b3006321e4207c0bdcf
B. D. <164544260+Barnelydd@users.noreply.github.com>
2025-11-16 20:44:46 -0400

    alive and kicking

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,33 @@ public class Main {
 	public static void main(String[] args) {
 
 	//Testing around and getting the hang of java
+
+			JFrame window = new JFrame();
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			window.setResizable(false);
+			window.setTitle("Heavens sang in ding dong dang");
+			GamePanel gamePanel = new GamePanel();
+			
+			window.add(gamePanel);
+			
+			window.setLocationRelativeTo(window);
+			window.setVisible(true);
+			window.pack();
+			
+			
+			/*
+			int[][] setiado = { '1920' , '1280'},
+					 {'2880', '1920'} ;
+					 
+					 
+					 //Luego le añadimos public a esta mrd que me está dando problemas
+					 //El truquito recién inventado como que no me va a funcionar eh
+					 final int ResY = setiado[1];
+					 final int ResX = setiado[2];
+					 
+					 System.out.println("La resolucion es de: " + ResY + " por " + ResX );
+			 * */
+
 	
 	}
 	public static void Box() {
@@ -18,6 +45,7 @@ public class Main {
 	}
 	public static void Inpt()
 	{
+		/*
 		Scanner scan = new Scanner(System.in);
 		System.out.println("What ya looking at?");
 		
@@ -26,6 +54,7 @@ public class Main {
 		
 		
 		System.out.println("You are looking at: " +  thinga);
+		 * */
 		
 		//String color1 = JOptionPane.showInputDialog("Is it blue?");
 		
@@ -33,20 +62,5 @@ public class Main {
 		
 	}	
 	
-	
-	public static void Frame()
-	{
-		
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setTitle("Heavens sang in ding dong dang");
-		
-		window.setLocationRelativeTo(window);
-		window.setVisible(true);
-
-		
-		
-	}
 
 }
--- src/main/PlayableCharacter.java
+++ src/main/PlayableCharacter.java
@@ -5,16 +5,7 @@ public class PlayableCharacter {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * */int[] setiado = {9 , 8 , 7};
-		
-		 
-		 //Luego le añadimos public a esta mrd que me está dando problemas
-		 //El truquito recién inventado como que no me va a funcionar eh
-		final int ResY = setiado[1];
-		final int ResX = setiado[2];
-		
-		System.out.println("La resolucion es de: " + ResY + " por " + ResX );
+
 		
 		
 
